5e45fc655136fd5ceab04db7e7eede069e6a51d9
Will <wveatchw@gmail.com>
2022-10-16 16:08:13 -0400

    working demo

--- app/Main.hs
+++ app/Main.hs
@@ -1,28 +1,74 @@
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Main where
 
-import FRP.Tigris
+-- tigris
+import Tigris
 
-import Control.Monad.IO.Class (liftIO)
+-- text
+import Data.Text
+
+-- rhine
+import FRP.Rhine
+import Control.Monad.Schedule
+
+-- base
+import Control.Concurrent
+
+-- apecs
+import Apecs
+import Apecs.Stores
+
 
 main :: IO ()
-main = do
-  msf <- eraseDelay tigrisMain
-  reactimate msf
+main = initAndRun "Game" gameLoop
 
-printWord :: DlSF IO dl () ()
-printWord = constM $ liftIO $ putStrLn "Hey"
-
-printOtherWord :: DlSF IO dl () ()
-printOtherWord = constM $ liftIO $ putStrLn "there"
-
-
+clsfLoop :: MonadIO m => ClSFS m (HoistClock IO (SystemT World m) (Millisecond 16)) () ()
+clsfLoop =
+  rotateTowardsMouse
+  >>> normVelocity
+  >>> setPosition
+  >>> updateDestination
+  >>> incFrame
 
 
-tigrisMain :: Tigris IO (ParallelDelay IO (Millisecond 1000) (Millisecond 5000)) () ()
-tigrisMain = Tigris
-  (Parallel
-    (Synchronous printWord)
-    (Synchronous printOtherWord))
-  (ParallelDelay waitDelay waitDelay concurrently)
+rhineLoop
+  :: World
+  -> RhineS IO (ParClockS IO 
+                (ParClockS IO (SeqClockS IO
+                                (HoistClock IO (SystemT World IO) Busy)
+                                (HoistClock IO (SystemT World IO) Busy)
+                              )
+                              (ParClockS IO (HoistClock IO (SystemT World IO) (Millisecond 16))
+                                (ParClockS IO
+                                  WindowResizeClock
+                                  (HoistClock IO (SystemT World IO) (Millisecond 16))
+                                )
+                              )
+                )
+                (HoistClock IO (SystemT World IO) (Millisecond 16))
+               )
+  () ()
+rhineLoop world =
+  (handleEvent world ||@ (concurrentlySystem world) @||
+    (clsfLoop @@ (HoistClock waitClock liftIO) ||@ (concurrentlySystem world) @||
+     Rhine cameraProcess 
+           (ParallelClock
+            WindowResizeClock
+             (HoistClock waitClock liftIO)
+             (concurrentlySystem world)
+           )
+    )
+  )
+  ||@ (concurrentlySystem world) @||
+  (copyAll >>> present) @@ (HoistClock waitClock liftIO)
+
+gameLoop :: World -> SystemT' IO ()
+gameLoop world = do
+  player
+  set global $ Camera $ mkRect 0 0 800 600
+  setReadOnly global $ TileMapSize $ V2 800 600
+  flow $ rhineLoop world
+
